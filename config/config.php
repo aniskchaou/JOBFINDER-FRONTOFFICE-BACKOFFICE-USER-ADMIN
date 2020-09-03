e245b4c96b73b33a1c63601faa28b17f3d97550f
Anis KCHAOU <kchaouanis25@gmail.com>
2020-09-03 16:17:44 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 
 //constants
-define("DB_HOST","localhost");
-define("DB_USER","postgres");
-define("DB_PASS","root");
-define("DB_NAME","jobs");
+define("DB_HOST","ec2-54-211-169-227.compute-1.amazonaws.com");
+define("DB_USER","wpistqbvrnqvdf");
+define("DB_PASS","e9b2abb1511eb28e2d41dc1d41a5219573648192df780f0a4c4c5ff842187491");
+define("DB_NAME","d47mjt3lklud2g");
 define("SITE_TITLE","JOBFINDER");
 
 
